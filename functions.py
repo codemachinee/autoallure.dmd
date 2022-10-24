b657a7dd414a3afe46784e77dad1fab3070886af
Igor <99876582+codemachinee@users.noreply.github.com>
2022-10-24 22:52:02 +0300

    1

---
+++ functions.py
@@ -0,0 +1,140 @@
+from telebot import types
+
+
+def marks_buttons(bot, message):
+    kb1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    but1 = types.KeyboardButton(text='AUDI')
+    but2 = types.KeyboardButton(text='BMW')
+    but3 = types.KeyboardButton(text='CADILLAC')
+    but4 = types.KeyboardButton(text='CHERY')
+    but5 = types.KeyboardButton(text='CHEVROLET')
+    but6 = types.KeyboardButton(text='CHRYSLER')
+    but7 = types.KeyboardButton(text='CITROEN')
+    but8 = types.KeyboardButton(text='EXCEED')
+    but9 = types.KeyboardButton(text='FORD')
+    but10 = types.KeyboardButton(text='GEELY')
+    but11 = types.KeyboardButton(text='HAVAL')
+    but12 = types.KeyboardButton(text='HONDA')
+    but13 = types.KeyboardButton(text='HYUNDAI')
+    but14 = types.KeyboardButton(text='INFINITI')
+    but15 = types.KeyboardButton(text='JAGUAR')
+    but16 = types.KeyboardButton(text='JEEP')
+    but17 = types.KeyboardButton(text='KIA')
+    but18 = types.KeyboardButton(text='LADA')
+    but19 = types.KeyboardButton(text='LAND ROVER')
+    but20 = types.KeyboardButton(text='LEXUS')
+    but21 = types.KeyboardButton(text='MAZDA')
+    but22 = types.KeyboardButton(text='MERCEDES-BENZ')
+    but23 = types.KeyboardButton(text='MINI')
+    but24 = types.KeyboardButton(text='MITSUBISHI')
+    but25 = types.KeyboardButton(text='NISSAN')
+    but26 = types.KeyboardButton(text='OPEL')
+    but27 = types.KeyboardButton(text='PEUGEOT')
+    but28 = types.KeyboardButton(text='PORSCHE')
+    but29 = types.KeyboardButton(text='RENAULT')
+    but30 = types.KeyboardButton(text='SKODA')
+    but31 = types.KeyboardButton(text='SUBARU')
+    but32 = types.KeyboardButton(text='TOYOTA')
+    but33 = types.KeyboardButton(text='VOLVO')
+    but34 = types.KeyboardButton(text='VOLKSWAGEN')
+    but35 = types.KeyboardButton(text='🚫Отсутствует в списке')
+    kb1.add(but1, but2, but3, but4, but5, but6, but7, but8, but9, but10, but11, but12, but13, but14, but15, but16,
+            but17, but18, but19, but20, but21, but22, but23, but24, but25, but26, but27, but28, but29, but30, but31,
+            but32, but33, but34, but35)
+    bot.send_message(message.chat.id, 'Пожалуйста выберите марку Вашего автомобиля', reply_markup=kb1)
+
+
+class search_models:
+    klass_first = ['TT', 'X2', 'Bonus', 'ATS', 'Aveo', 'Spark', 'Neon', 'DS-4', 'Focus', 'Jazz', 'Atos', 'Q30', 'Rio', 'Granta',
+                   'IS,', 'MX-5', 'SLK', 'Cooper', 'Colt', 'Tiida', 'Astra', '308', '911', 'Clio', 'Rapid', 'Legacy',
+                   'Yaris', 'S40', 'Polo']
+    klass_second = ['RS6', 'X4', 'Tiggo 1-4', 'XT5', 'Rezzo', 'Voyager', 'Picasso', 'TXL', 'Kuga', 'Coolray',
+                    'Dargo', 'Prelude', 'Creta', 'Tucson', 'QX55', 'F-type', 'Venga', 'Evoque', 'GS', 'CX-3', 'GLA',
+                    'Clubman', 'Grandis', 'Juke', 'Omega', 'Partner', 'Macan', 'Arkana', 'Laguna', 'Yeti', 'Impreza', 'Prius',
+                    'V60', 'Tiguan']
+    klass_third = ['e-tron', 'X6', 'Tiggo 7-8', 'Escalade', 'Tahoe', 'C-crosser', 'VX', 'Galaxy', 'Tugella',
+                   'H9', 'CR-V', 'Palisade', 'JX70', 'XJ', 'Compass', 'Mohave', 'Velar', 'LX', 'CX-9', 'GLC', 'Pajero',
+                   'Murano', 'Cayenne', 'Koleos', 'Kodiaq', 'Tribeca', 'Prado', 'XC90', 'Phaeton']
+    klasses = [klass_first, klass_second, klass_third]
+
+    def __init__(self, bot, message, text, auto_model):
+        self.bot = bot
+        self.message = message
+        self.text = text
+        self.list = list
+        self.auto_model = auto_model
+        for klass in self.klasses:
+            for i in klass:
+                if text.find(i) >= 0 and klass == self.klass_first:
+                    file_open = open("1 class_.png", 'rb')
+                    bot.send_photo(message.chat.id, file_open, f'Готово!\n'
+                                                               f'Стоимость услуг для Вашего автомобиля {auto_model}\n'
+                                                               'соответствует первому ценовому классу.')
+                    model_buttons(self.bot, self.message).zayavka_buttons()
+                    bot.send_message('1338281106', f'Хозяин! Замечена активность:\n'
+                                                   f'Имя: {message.from_user.first_name}\n'
+                                                   f'Фамилия: {message.from_user.last_name}\n'
+                                                   f'Псевдоним: @{message.from_user.username}\n'
+                                                   f'Авто: {auto_model} 1 класса')
+
+                if text.find(i) >= 0 and klass == self.klass_second:
+                    file_open = open("2 class.png", 'rb')
+                    bot.send_photo(message.chat.id, file_open, f'Готово!\n'
+                                                               f'Стоимость услуг для Вашего автомобиля {auto_model}\n'
+                                                               'соответствует второму ценовому классу.')
+                    model_buttons(self.bot, self.message).zayavka_buttons()
+                    bot.send_message('1338281106', f'Хозяин! Замечена активность:\n'
+                                                   f'Имя: {message.from_user.first_name}\n'
+                                                   f'Фамилия: {message.from_user.last_name}\n'
+                                                   f'Псевдоним: @{message.from_user.username}\n'
+                                                   f'Авто: {auto_model} 2 класса')
+
+                if text.find(i) >= 0 and klass == self.klass_third:
+                    file_open = open("3 class.png", 'rb')
+                    bot.send_photo(message.chat.id, file_open, f'Готово!\n'
+                                                               f'Стоимость услуг для Вашего автомобиля {auto_model}\n'
+                                                               'соответствует третьему ценовому классу.')
+                    model_buttons(self.bot, self.message).zayavka_buttons()
+                    bot.send_message('1338281106', f'Хозяин! Замечена активность:\n'
+                                                   f'Имя: {message.from_user.first_name}\n'
+                                                   f'Фамилия: {message.from_user.last_name}\n'
+                                                   f'Псевдоним: @{message.from_user.username}\n'
+                                                   f'Авто: {auto_model} 3 класса')
+
+
+class model_buttons:
+
+    def __init__(self, bot, message, **kwargs):
+        self.bot = bot
+        self.message = message
+        self.kwargs = kwargs
+
+    def model_buttons(self):
+        kb3 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        for key, value in self.kwargs.items():
+            key = types.KeyboardButton(text=f'{value}')
+            kb3.add(key)
+        self.bot.send_message(self.message.chat.id, 'Пожалуйста выберите модель Вашего автомобиля', reply_markup=kb3)
+
+    def zayavka_buttons(self):
+        kb4 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        but1 = types.KeyboardButton(text='Да, хочу!')
+        but2 = types.KeyboardButton(text='🔙Вернуться в начало')
+        kb4.add(but1, but2)
+        self.bot.send_message(self.message.chat.id, 'Хотите оставить заявку на интересующую(-ие) Вас услугу(-и)?',
+                              reply_markup=kb4)
+
+
+def zayavka_done(bot, message):
+    kb2 = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, f'Заявка оформлена и передана мастеру, с Вами свяжутся в ближайшее время. '
+                                      'Спасибо, что выбрали нас.🤝\n'
+                                      f'Для нового рассчета воспользуйтесь командой /price', reply_markup=kb2)
+    bot.send_message('1338281106', f'🚨!!!СРОЧНО!!!🚨\n'
+                                   f'Хозяин, поступила ЗАЯВКА от:\n'
+                                   f'Псевдоним: @{message.from_user.username}\n'
+                                   f'Быстрее согласуй дату и закрой заявку пока он не слился')
+
+
+
+
---
+++ main.py
@@ -0,0 +1,221 @@
+import telebot
+from telebot import types
+from functions import marks_buttons, model_buttons, search_models, zayavka_done
+
+token = '5380562272:AAFqodiUpENCtx7oD8f5xnbIDNOoxJW6YMY'
+bot = telebot.TeleBot(token)
+
+auto_model = None
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    kb2 = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, '...', reply_markup=kb2)
+    file_open = open("start_logo.png", 'rb')
+    bot.send_photo(message.chat.id, file_open, '''Здравствуйте!
+Вас приветствует autoallure.dmd_bot - надежный сервис и помощник по уходу за Вашим автомобилем .﻿🚘
+
+/price - рассчет цены на услуги autoallure для Вашего авто
+/help - все возможности бота''')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    kb2 = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, '...', reply_markup=kb2)
+    bot.send_message(message.chat.id, f'Основные команды поддерживаемые ботом:\n'
+                                      f'/price -  рассчет услуг для любого авто\n'
+                                      f'/start - инициализация бота\n'
+                                      f'/help - справка по боту\n')
+
+
+@bot.message_handler(commands=['price'])
+def price(message):
+    marks_buttons(bot, message)
+
+
+@bot.message_handler(func=lambda m: m.text)
+def chek_message_auto(m):
+    global auto_model
+    if m.text == '🔙Вернуться в начало':
+        marks_buttons(bot, m)
+    if m.text == '🚫Отсутствует в списке':
+        sent = bot.send_message(m.chat.id, 'Пожалуйста, введите марку и модель авто с помощью клавиатуры...')
+        bot.register_next_step_handler(sent, redkoe_auto)
+    if m.text == 'Да, хочу!':
+        zayavka_done(bot=bot, message=m)
+    if m.text == 'AUDI':
+        auto_model = 'AUDI'
+        model_buttons(bot=bot, message=m, but1='A1, A2, A3, TT, A4, A5', but2='A6, A7, RS6, Q3, Q5, A8, R8',
+                      but3='Q7, Q8, e-tron', but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'BMW':
+        auto_model = 'BMW'
+        model_buttons(bot=bot, message=m, but1='2, 3, 4, Z, X1, X2', but2='5, 6, 7, X3, X4',
+                      but3='X5, X6, X7', but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'CHERY':
+        auto_model = 'CHERY'
+        model_buttons(bot=bot, message=m, but1='Amulet, Bonus, E5, Fora, Very', but2='Tiggo 1-4',
+                      but3='Tiggo 7-8', but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'CADILLAC':
+        auto_model = 'CADILLAC'
+        model_buttons(bot=bot, message=m, but1='CTS, ATS, BLS', but2='SRX, STS, XT4, XT5',
+                      but3='Escalade, XT6', but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'CHEVROLET':
+        auto_model = 'CHEVROLET'
+        model_buttons(bot=bot, message=m, but1='Aveo, Corvette, Camaro, Lacetti, Malibu',
+                      but2='Spark, Niva, Cruze, Volt', but3='Evica, Orlando, Rezzo, Captiva',
+                      but4='TrailBlazer, Tahoe, Traverse', but5='🚫Отсутствует в списке',
+                      but6='🔙Вернуться в начало').model_buttons()
+    if m.text == 'CHRYSLER':
+        auto_model = 'CHRYSLER'
+        model_buttons(bot=bot, message=m, but1='Neon, Sebring, Stratus, PT Cruiser',
+                      but2='300C, Grand Voyager, Pacifica', but3='🚫Отсутствует в списке',
+                      but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'CITROEN':
+        auto_model = 'CITROEN'
+        model_buttons(bot=bot, message=m, but1='C1, C2, C3, C4, DS-4', but2='C6, Picasso, Berlingo, C5, DS-5',
+                      but3='C-crosser', but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'EXCEED':
+        auto_model = 'EXCEED'
+        model_buttons(bot=bot, message=m, but1='LX, TXL', but2='VX', but3='🚫Отсутствует в списке',
+                      but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'GEELY':
+        auto_model = 'GEELY'
+        model_buttons(bot=bot, message=m, but1='Coolray', but2='Tugella, Atlas', but3='🚫Отсутствует в списке',
+                      but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'FORD':
+        auto_model = 'FORD'
+        model_buttons(bot=bot, message=m, but1='Fusion, Focus, Fiesta, Mustang',
+                      but2='Mondeo, Kuga, Maverick, Escape, S-Max', but3='Galaxy, Explorer',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'HAVAL':
+        auto_model = 'HAVAL'
+        model_buttons(bot=bot, message=m, but1='Dargo', but2='F7, H6, H8, H9', but3='🚫Отсутствует в списке',
+                      but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'HONDA':
+        auto_model = 'HONDA'
+        model_buttons(bot=bot, message=m, but1='Accord, Jazz, Civic', but2='Crosstour, Legend, Element, HR-V, Prelude',
+                      but3='CR-V', but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'HYUNDAI':
+        auto_model = 'HYUNDAI'
+        model_buttons(bot=bot, message=m, but1='Accent, Getz, I30, I40, Atos, Solaris',
+                      but2='Elantra, Sonata, Creta, Eques', but3='Genesis, Tucson, IX35, Matrix',
+                      but4='Santa Fe, Palisade, IX55, Genesis GV70', but5='🚫Отсутствует в списке',
+                      but6='🔙Вернуться в начало').model_buttons()
+    if m.text == 'INFINITI':
+        auto_model = 'INFINITI'
+        model_buttons(bot=bot, message=m, but1='G, Q30', but2='FX, Q50, Q70, QX30, QX50, QX55', but3='JX70, QX70',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+
+    if m.text == 'JAGUAR':
+        auto_model = 'JAGUAR'
+        model_buttons(bot=bot, message=m, but1='XF, F-type, F-pace', but2='XJ', but3='🚫Отсутствует в списке',
+                      but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'JEEP':
+        auto_model = 'JEEP'
+        model_buttons(bot=bot, message=m, but1='Wrangler, Liberty Compass, Cherokee', but2='🚫Отсутствует в списке',
+                      but3='🔙Вернуться в начало').model_buttons()
+    if m.text == 'KIA':
+        auto_model = 'KIA'
+        model_buttons(bot=bot, message=m, but1='Ceed, Cerato, Rio, Picanto',
+                      but2='Optima, K5, K8, K900, Seltos, Sportage, Venga, Soul', but3='Carnival, Mohave, Sorento',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'LADA':
+        auto_model = 'LADA'
+        model_buttons(bot=bot, message=m, but1='Granta, Kalina, Priora, Vesta, Largus, Niva, XRAY',
+                      but2='🚫Отсутствует в списке', but3='🔙Вернуться в начало').model_buttons()
+    if m.text == 'LAND ROVER':
+        auto_model = 'LAND ROVER'
+        model_buttons(bot=bot, message=m, but1='Freelander, Evoque', but2='Defender, Discovery, Range Rover, Velar',
+                      but3='🚫Отсутствует в списке', but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'LEXUS':
+        auto_model = 'LEXUS'
+        model_buttons(bot=bot, message=m, but1='IS, CT, LC', but2='RX, NX, ES, GS', but3='LS, LX, GX',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'MAZDA':
+        auto_model = 'MAZDA'
+        model_buttons(bot=bot, message=m, but1='2, 3, MX-5', but2='5, 6, CX-3, CX-5', but3='CX-7, CX-9',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'MERCEDES-BENZ':
+        auto_model = 'MERCEDES-BENZ'
+        model_buttons(bot=bot, message=m, but1='A, B, C, SLK, CLK, CLA', but2='E, GLK, GLA, SL, CLS, ML',
+                      but3='GLE, GLC, R, S, CL', but4='🚫Отсутствует в списке',
+                      but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'MINI':
+        auto_model = 'MINI'
+        model_buttons(bot=bot, message=m, but1='Cooper',
+                      but2='Countryman, Clubman', but3='🚫Отсутствует в списке',
+                      but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'MITSUBISHI':
+        auto_model = 'MITSUBISHI'
+        model_buttons(bot=bot, message=m, but1='Eclipse, Colt, Galant, Lancer',
+                      but2='ASX, Eclipse Cross, Grandis, Space Star, Outlander',
+                      but3='L-200, Outlander XL, Pajero, Pajero Sport', but4='🚫Отсутствует в списке',
+                      but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'NISSAN':
+        auto_model = 'NISSAN'
+        model_buttons(bot=bot, message=m, but1='Almera, Note, Tiida, Maxima, Micra', but2='Juke, Qashqai, 350Z, GT-R',
+                      but3='Murano, Teana, X-Trail, Pathfinder, Patrol', but4='🚫Отсутствует в списке',
+                      but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'OPEL':
+        auto_model = 'OPEL'
+        model_buttons(bot=bot, message=m, but1='Astra, Corsa, Insignia', but2='Zafira, Omega, Vectra, Meriva',
+                      but3='🚫Отсутствует в списке', but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'PEUGEOT':
+        auto_model = 'PEUGEOT'
+        model_buttons(bot=bot, message=m, but1='107, 108, 206, 207, 3001, 308', but2='2008, 4008, 407, 508, Partner',
+                      but3='🚫Отсутствует в списке', but4='🔙Вернуться в начало').model_buttons()
+    if m.text == 'PORSCHE':
+        auto_model = 'PORSCHE'
+        model_buttons(bot=bot, message=m, but1='911, Cayman', but2='Macan, Panamera', but3='Cayenne',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'RENAULT':
+        auto_model = 'RENAULT'
+        model_buttons(bot=bot, message=m, but1='Clio, Megane, Logan, Symbol, Sandero',
+                      but2='Arkana, Captur, Duster, Kangoo, ', but3='Fluence, Scenic, Talisman, Laguna',
+                      but4='Koleos', but5='🚫Отсутствует в списке', but6='🔙Вернуться в начало').model_buttons()
+    if m.text == 'SKODA':
+        auto_model = 'SKODA'
+        model_buttons(bot=bot, message=m, but1='Fabia, Rapid, Ibiza', but2='Octavia, Karoq, Superb, Roomster, Yeti',
+                      but3='Kodiaq', but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'SEAT':
+        auto_model = 'SEAT'
+        model_buttons(bot=bot, message=m, but1='Altea, Freetrack, Ibiza, Leon', but2='🚫Отсутствует в списке',
+                      but3='🔙Вернуться в начало').model_buttons()
+    if m.text == 'SUBARU':
+        auto_model = 'SUBARU'
+        model_buttons(bot=bot, message=m, but1='Legacy', but2='Ascent, Forester, Outback, Impreza', but3='Tribeca',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'TOYOTA':
+        auto_model = 'TOYOTA'
+        model_buttons(bot=bot, message=m, but1='Auris, Corolla, Yaris',
+                      but2='Camry, Avensis, C-HR, Prius, Versa, GT86, Crown',
+                      but3='Fortuner, LC100-300, Prado, Highlander', but4='🚫Отсутствует в списке',
+                      but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'VOLVO':
+        auto_model = 'VOLVO'
+        model_buttons(bot=bot, message=m, but1='C30, S40, V40', but2='C40, CC, S60, S80, S90, V50, V60, V70, XC40',
+                      but3='XC60, XC70, XC90', but4='🚫Отсутствует в списке',
+                      but5='🔙Вернуться в начало').model_buttons()
+    if m.text == 'VOLKSWAGEN':
+        auto_model = 'VOLKSWAGEN'
+        model_buttons(bot=bot, message=m, but1='Polo, Scriocco, Beetle, Jetta, Golf',
+                      but2='Passat, Arteon, Touran, Sharan, Golf plus, Tiguan', but3='Phaeton, Touareg',
+                      but4='🚫Отсутствует в списке', but5='🔙Вернуться в начало').model_buttons()
+    search_models(bot, m, m.text, auto_model=auto_model)
+
+
+def redkoe_auto(message):
+    bot.send_message(message.chat.id, 'Cпасибо! Я передал информацию мастеру. Прайс будет выслан Вам в ближайшее '
+                                      'время.')
+    bot.send_message('1338281106', f'🚨!!!СРОЧНО!!!🚨\n'
+                                   f'Хозяин, поступил запрос прайса на отсутствующее в моем списке авто от:\n'
+                                   f'Имя: {message.from_user.first_name}\n'
+                                   f'Фамилия: {message.from_user.last_name}\n'
+                                   f'Псевдоним: @{message.from_user.username}\n'
+                                   f'Авто: {message.text}\n'
+                                   f'Быстрее отправь прайс на его корыто пока он не слился')
+
+
+bot.infinity_polling()
